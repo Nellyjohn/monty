bd61222322d8d8937d1706c1590161958c3b9091
Nellyjohn <okpala_vivian@.com>
2023-12-13 07:43:00 -0800

    betty update

--- _is_pall.c
+++ _is_pall.c
@@ -1,5 +1,12 @@
 #include "monty.h"
 
+/**
+  *  _is_pall - prints all the values on the stack,
+  * starting from the top of the stack
+  * @h: head of the list
+  * @line_number: bytecode line number
+  * Return: nothing
+  */
 void _is_pall(stack_t **h, unsigned int line_number)
 {
 	stack_t *tmp = NULL;
