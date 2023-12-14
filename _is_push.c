7da89b40694064253031af0eada1a6858aacffe6
Nellyjohn <okpala_vivian@.com>
2023-12-14 12:49:55 -0800

    Debugging

--- _is_push.c
+++ _is_push.c
@@ -36,7 +36,7 @@ void _is_push(stack_t **h, unsigned int line_number, const char *n)
 
 	if (_is_number(n) == -1)
 	{
-		printf("L%u: usage: push integer\n", line_number);
+		printf("L%d: usage: push integer\n", line_number);
 		_is_free_dlist(h);
 		exit(EXIT_FAILURE);
 	}
@@ -44,7 +44,7 @@ void _is_push(stack_t **h, unsigned int line_number, const char *n)
 	num = atoi(n);
 	if (num == 0 && n[0] != '0')
 	{
-		printf("L%u: usage: push integer\n", line_number);
+		printf("L%d: usage: push integer\n", line_number);
 		_is_free_dlist(h);
 		exit(EXIT_FAILURE);
 	}
